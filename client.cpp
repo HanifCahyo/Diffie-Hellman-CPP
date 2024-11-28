05ee993ea70e1c333fe7a048ed60718f53bcdb9a
HanifCahyo <hanifcahyoprasetyo95@gmail.com>
2024-11-28 19:30:44 +0700

    feat: looping message

--- client.cpp
+++ client.cpp
@@ -8,30 +8,25 @@
 #include <openssl/pem.h>
 #include <openssl/dh.h>
 #include <openssl/rand.h>
-#include <openssl/err.h>   // For error handling
+#include <openssl/err.h>
 
 using namespace std;
 
-int main() {
-    cout << "Creating socket..." << endl;
-    int clientSocket = socket(AF_INET, SOCK_STREAM, 0);
-    if (clientSocket == -1) {
+int main() {`
+    cout << "Creating socket..." << endl; // Print a message to the console
+    int clientSocket = socket(AF_INET, SOCK_STREAM, 0); // Create a new socket using IPv4 and TCP 
+    if (clientSocket == -1) { // Check if the socket was created successfully
         cerr << "Failed to create socket." << endl;
         return -1;
     }
 
-    sockaddr_in serverAddress;
-    serverAddress.sin_family = AF_INET;
-    serverAddress.sin_port = htons(8080);
+    sockaddr_in serverAddress; // Create a new sockaddr_in struct
+    serverAddress.sin_family = AF_INET; // Set the address family to IPv4
+    serverAddress.sin_port = htons(8080);  // Set the port to 8080
+    inet_pton(AF_INET, "127.0.0.1", &serverAddress.sin_addr); // Set the IP address to localhost
 
-    if (inet_pton(AF_INET, "127.0.0.1", &serverAddress.sin_addr) <= 0) {
-        cerr << "Invalid address." << endl;
-        close(clientSocket);
-        return -1;
-    }
-
-    cout << "Connecting to server..." << endl;
-    if (connect(clientSocket, (struct sockaddr *)&serverAddress, sizeof(serverAddress)) == -1) {
+    cout << "Connecting to server..." << endl; // Print a message to the console
+    if (connect(clientSocket, (struct sockaddr *)&serverAddress, sizeof(serverAddress)) == -1) { // Connect to the server
         cerr << "Failed to connect to server." << endl;
         close(clientSocket);
         return -1;
@@ -39,137 +34,85 @@ int main() {
 
     cout << "Connected to server." << endl;
 
-    // Generate client's Diffie-Hellman key pair
-    // Gantikan pembuatan objek DH dengan menggunakan parameter standar
-    DH* dh = DH_get_2048_256();
-    if (!dh) {
-        cerr << "Failed to create Diffie-Hellman object." << endl;
-        close(clientSocket);
-        return -1;
-    }
+    // Diffie-Hellman key exchange
+    DH* dh = DH_get_2048_256(); // Create a new Diffie-Hellman key exchange object
+    DH_generate_key(dh); // Generate the DH keys
 
-    if (DH_generate_key(dh) != 1) {
-        cerr << "Failed to generate DH key pair." << endl;
-        DH_free(dh);
-        close(clientSocket);
-        return -1;
-    }
+    const BIGNUM *client_pub_key = nullptr; // Create a new BIGNUM pointer
+    DH_get0_key(dh, &client_pub_key, nullptr); // Get the client's public key
+    int pub_key_len = BN_num_bytes(client_pub_key); // Get the length of the public key
+    unsigned char *client_pub_key_bytes = new unsigned char[pub_key_len]; // Create a new byte array to store the public key
+    BN_bn2bin(client_pub_key, client_pub_key_bytes); // Convert the public key to bytes
 
-    const BIGNUM *pub_key = nullptr;
-    DH_get0_key(dh, &pub_key, nullptr);
-
-    // Send client's public key to the server
-    int pub_key_len = BN_num_bytes(pub_key);
-    unsigned char *pub_key_bytes = new unsigned char[pub_key_len];
-    BN_bn2bin(pub_key, pub_key_bytes);
-
-    ssize_t sentBytes = send(clientSocket, pub_key_bytes, pub_key_len, 0);
-    if (sentBytes == -1) {
-        cerr << "Failed to send public key to server." << endl;
-        delete[] pub_key_bytes;
-        DH_free(dh);
-        close(clientSocket);
-        return -1;
-    }
-
-    cout << "Sent client's public key to server." << endl;
+    // Send client's public key to server
+    send(clientSocket, client_pub_key_bytes, pub_key_len, 0); // Send the public key to the server
 
     // Receive server's public key
-    unsigned char buffer[2048];
-    ssize_t bytesReceived = recv(clientSocket, buffer, sizeof(buffer), 0);
-    if (bytesReceived <= 0) {
-        cerr << "Failed to receive public key from server." << endl;
-        delete[] pub_key_bytes;
-        DH_free(dh);
-        close(clientSocket);
-        return -1;
-    }
-    cout << "Received server's public key." << endl;
+    unsigned char server_pub_key_bytes[2048]; // Create a new byte array to store the server's public key
+    ssize_t bytesReceived = recv(clientSocket, server_pub_key_bytes, sizeof(server_pub_key_bytes), 0); // Receive the server's public key
+    BIGNUM *server_pub_key = BN_bin2bn(server_pub_key_bytes, bytesReceived, nullptr); // Convert the server's public key to a BIGNUM
 
     // Compute shared secret
-    BIGNUM *server_pub_key = BN_bin2bn(buffer, bytesReceived, nullptr);
-    unsigned char shared_secret[EVP_MAX_KEY_LENGTH];
+    unsigned char shared_secret[EVP_MAX_KEY_LENGTH]; // Create a new byte array to store the shared secret
+    DH_compute_key(shared_secret, server_pub_key, dh); // Compute the shared secret using the server's public key and the DH object
 
-    int secret_len = DH_compute_key(shared_secret, server_pub_key, dh);
-    if (secret_len <= 0) {
-        cerr << "Failed to compute shared secret." << endl;
-        delete[] pub_key_bytes;
-        DH_free(dh);
-        close(clientSocket);
-        return -1;
+    cout << "Shared secret computed." << endl; // Print a message to the console
+    delete[] client_pub_key_bytes; // Delete the client's public key bytes
+
+    // Main chat loop
+    while (true) { // Loop until the user types "exit"
+        // Get client message
+        cout << "Client: "; // Print a message to the console
+        string message; // Create a new string to store the message
+        getline(cin, message); // Get a line of input from the user
+
+        if (message == "exit") break;
+
+        // Encrypt and send message
+        unsigned char iv[EVP_MAX_IV_LENGTH]; // Create a new byte array to store the Initialization Vector
+        RAND_bytes(iv, sizeof(iv)); // Generate a random IV
+        EVP_CIPHER_CTX *encrypt_ctx = EVP_CIPHER_CTX_new(); // Create a new EVP cipher context
+        EVP_EncryptInit_ex(encrypt_ctx, EVP_aes_256_cbc(), nullptr, s hared_secret, iv); // Initialize the encryption context
+
+        unsigned char encrypted_message[128]; // Create a new byte array to store the encrypted message
+        int encrypted_len; // Create a new integer to store the length of the message
+        int len; // Create a new integer to store the length of the message
+        EVP_EncryptUpdate(encrypt_ctx, encrypted_message, &len, (unsigned char*)message.c_str(), message.size()); // Encrypt the message
+        encrypted_len = len; // Set the encrypted length
+
+        EVP_EncryptFinal_ex(encrypt_ctx, encrypted_message + len, &len); // Finalize the encryption
+        encrypted_len += len; // Update the encrypted length
+
+        send(clientSocket, iv, sizeof(iv), 0); // Send the IV to the server
+        send(clientSocket, encrypted_message, encrypted_len, 0); // Send the encrypted message to the server
+
+        EVP_CIPHER_CTX_free(encrypt_ctx); // Free the encryption context
+
+        // Receive and decrypt message from server
+        unsigned char iv_in[EVP_MAX_IV_LENGTH]; // Create a new byte array to store the Initialization Vector
+        ssize_t ivReceived = recv(clientSocket, iv_in, sizeof(iv_in), 0); // Receive the IV from the server
+        if (ivReceived <= 0) break; // Check if the IV was received successfully
+
+        unsigned char encrypted_in[128]; // Create a new byte array to store the encrypted message
+        ssize_t encrypted_in_len = recv(clientSocket, encrypted_in, sizeof(encrypted_in), 0); // Receive the encrypted message from the server
+        if (encrypted_in_len <= 0) break; // Check if the encrypted message was received successfully
+
+        unsigned char decrypted_message[128]; // Create a new byte array to store the decrypted message
+        EVP_CIPHER_CTX *decrypt_ctx = EVP_CIPHER_CTX_new(); // Create a new EVP cipher context
+        EVP_DecryptInit_ex(decrypt_ctx, EVP_aes_256_cbc(), nullptr, shared_secret, iv_in); // Initialize the decryption context
+
+        if (EVP_DecryptUpdate(decrypt_ctx, decrypted_message, &len, encrypted_in, encrypted_in_len) == 1) { // Decrypt the message
+            int decrypted_len = len;    // Store the length of the decrypted message
+            if (EVP_DecryptFinal_ex(decrypt_ctx, decrypted_message + len, &len) == 1) { // Finalize the decryption process
+                decrypted_len += len; // Update the length of the decrypted message
+                decrypted_message[decrypted_len] = '\0'; // Add a null terminator to the decrypted message
+                cout << "Server: " << decrypted_message << endl; // Print the decrypted message to the console
+            } 
+        }
+        EVP_CIPHER_CTX_free(decrypt_ctx);
     }
-
-    // Print shared secret for debugging
-    cout << "Server's shared secret: ";
-    for (int i = 0; i < secret_len; i++) {
-        printf("%02x", shared_secret[i]);
-    }
-    cout << endl;
-
-    cout << "Shared secret computed." << endl;
-
-    // Encrypt a message using the shared secret
-    unsigned char iv[EVP_MAX_IV_LENGTH];
-    if (RAND_bytes(iv, sizeof(iv)) != 1) {
-        cerr << "Failed to generate IV." << endl;
-        delete[] pub_key_bytes;
-        DH_free(dh);
-        close(clientSocket);
-        return -1;
-    }
-
-    EVP_CIPHER_CTX *encrypt_ctx = EVP_CIPHER_CTX_new();
-    EVP_EncryptInit_ex(encrypt_ctx, EVP_aes_256_cbc(), nullptr, shared_secret, iv);
-
-    unsigned char encrypted_message[128];
-    int len, encrypted_len;
-    if (EVP_EncryptUpdate(encrypt_ctx, encrypted_message, &len, (unsigned char *)"Hello, Server!", 15) != 1) {
-        cerr << "Encryption failed during update." << endl;
-        ERR_print_errors_fp(stderr);
-        EVP_CIPHER_CTX_free(encrypt_ctx);
-        delete[] pub_key_bytes;
-        DH_free(dh);
-        close(clientSocket);
-        return -1;
-    }
-    encrypted_len = len;
-
-    if (EVP_EncryptFinal_ex(encrypt_ctx, encrypted_message + len, &len) != 1) {
-        cerr << "Encryption failed during finalization." << endl;
-        ERR_print_errors_fp(stderr);
-        EVP_CIPHER_CTX_free(encrypt_ctx);
-        delete[] pub_key_bytes;
-        DH_free(dh);
-        close(clientSocket);
-        return -1;
-    }
-
-    encrypted_len += len;
-
-    // Send IV and encrypted message
-    if (send(clientSocket, iv, sizeof(iv), 0) == -1) {
-        cerr << "Failed to send IV." << endl;
-        EVP_CIPHER_CTX_free(encrypt_ctx);
-        delete[] pub_key_bytes;
-        DH_free(dh);
-        close(clientSocket);
-        return -1;
-    }
-
-    if (send(clientSocket, encrypted_message, encrypted_len, 0) == -1) {
-        cerr << "Failed to send encrypted message." << endl;
-        EVP_CIPHER_CTX_free(encrypt_ctx);
-        delete[] pub_key_bytes;
-        DH_free(dh);
-        close(clientSocket);
-        return -1;
-    }
-
-    cout << "Sent encrypted message to server." << endl;
 
     // Cleanup
-    EVP_CIPHER_CTX_free(encrypt_ctx);
-    delete[] pub_key_bytes;
     DH_free(dh);
     close(clientSocket);
 
--- server.cpp
+++ server.cpp
@@ -1,191 +1,156 @@
-#include <iostream>
-#include <cstring>
+#include <iostream> // Include the necessary libraries
+#include <cstring> // Include the necessary libraries
 #include <unistd.h>
 #include <netinet/in.h>
 #include <sys/socket.h>
 #include <arpa/inet.h>
 #include <openssl/evp.h>
 #include <openssl/pem.h>
-#include <openssl/dh.h>
+#include <openssl/dh.h> 
 #include <openssl/rand.h>
-#include <openssl/err.h>   // For error handling
+#include <openssl/err.h>
 
 using namespace std;
 
-int main() {
-    cout << "Creating socket..." << endl;
-    int serverSocket = socket(AF_INET, SOCK_STREAM, 0);
-    if (serverSocket == -1) {
-        cerr << "Failed to create socket." << endl;
-        return -1;
+int main() { // Create the main function 
+    cout << "Creating socket..." << endl; // Print a message to the console 
+    int serverSocket = socket(AF_INET, SOCK_STREAM, 0); // Create a new socket using IPv4 and TCP 
+    if (serverSocket == -1) { // Check if the socket was created successfully 
+        cerr << "Failed to create socket." << endl; // Print an error message
+        return -1; // Return an error code
     }
 
-    sockaddr_in serverAddress;
-    serverAddress.sin_family = AF_INET;
-    serverAddress.sin_port = htons(8080);
-    serverAddress.sin_addr.s_addr = INADDR_ANY;
+    sockaddr_in serverAddress; // Create a new sockaddr_in struct
+    serverAddress.sin_family = AF_INET; // Menentukan keluarga alamat sebagai AF_INET (IPv4).
+    serverAddress.sin_port = htons(8080); // Menentukan port server (8080). Fungsi htons() digunakan untuk mengonversi port ke urutan byte yang benar.
+    serverAddress.sin_addr.s_addr = INADDR_ANY; // Membuat server mendengarkan pada semua antarmuka jaringan yang tersedia.
 
-    cout << "Binding socket..." << endl;
-    if (bind(serverSocket, (struct sockaddr *)&serverAddress, sizeof(serverAddress)) == -1) {
-        cerr << "Failed to bind socket." << endl;
+    cout << "Binding socket..." << endl; // Print a message to the console
+    if (bind(serverSocket, (struct sockaddr *)&serverAddress, sizeof(serverAddress)) == -1) { // Bind the socket to the address
+        cerr << "Failed to bind socket." << endl;  // Print an error message
+        close(serverSocket);  // Close the socket
+        return -1; 
+    }
+
+    cout << "Listening on port 8080..." << endl; // Print a message to the console
+    if (listen(serverSocket, 1) == -1) { // Memulai server untuk mendengarkan koneksi masuk. Jumlah koneksi yang diizinkan adalah 1.
+        cerr << "Failed to listen on socket." << endl; // Print an error message
         close(serverSocket);
         return -1;
     }
 
-    cout << "Listening on port 8080..." << endl;
-    if (listen(serverSocket, 1) == -1) {
-        cerr << "Failed to listen on socket." << endl;
-        close(serverSocket);
+    sockaddr_in clientAddress; // Create a new sockaddr_in struct for the client address 
+    socklen_t clientAddressLen = sizeof(clientAddress); // Get the size of the client address struct 
+    cout << "Waiting for a connection..." << endl; // Print a message to the console 
+    int clientSocket = accept(serverSocket, (struct sockaddr *)&clientAddress, &clientAddressLen); // Accept a new connection from a client
+    if (clientSocket == -1) { // Check if the connection was accepted successfully 
+        cerr << "Failed to accept client connection." << endl;  // Print an error message
+        close(serverSocket); // Close the server socket
         return -1;
     }
 
-    sockaddr_in clientAddress;
-    socklen_t clientAddressLen = sizeof(clientAddress);
-    cout << "Waiting for a connection..." << endl;
-    int clientSocket = accept(serverSocket, (struct sockaddr *)&clientAddress, &clientAddressLen);
-    if (clientSocket == -1) {
-        cerr << "Failed to accept client connection." << endl;
-        close(serverSocket);
+    cout << "Client connected!" << endl; // Print a message to the console 
+
+    // Diffie-Hellman key exchange
+    DH* dh = DH_get_2048_256(); // Create a new Diffie-Hellman key exchange object
+    if (!dh || DH_generate_key(dh) != 1) { // Generate the DH keys
+        cerr << "Failed to create or generate DH keys." << endl; // Print an error message
+        close(clientSocket); 
+        close(serverSocket); 
         return -1;
     }
 
-    cout << "Client connected!" << endl;
-
-    // Receive client's public key
-    unsigned char buffer[2048];
-    ssize_t bytesReceived = recv(clientSocket, buffer, sizeof(buffer), 0);
-    if (bytesReceived <= 0) {
-        cerr << "Failed to receive public key from client." << endl;
-        close(clientSocket);
-        close(serverSocket);
-        return -1;
-    }
-    cout << "Received public key from client." << endl;
-
-    // Generate server's Diffie-Hellman key pair
-    DH* dh = DH_get_2048_256();
-    if (!dh) {
-        cerr << "Failed to create Diffie-Hellman object." << endl;
-        close(clientSocket);
-        close(serverSocket);
-        return -1;
-    }
-
-    if (DH_generate_key(dh) != 1) {
-        cerr << "Failed to generate DH key pair." << endl;
-        DH_free(dh);
-        close(clientSocket);
-        close(serverSocket);
-        return -1;
-    }
-
-    const BIGNUM *pub_key = nullptr;
-    DH_get0_key(dh, &pub_key, nullptr);
+    const BIGNUM *server_pub_key = nullptr; // Create a new BIGNUM pointer
+    DH_get0_key(dh, &server_pub_key, nullptr); // Get the server's public key
+    int pub_key_len = BN_num_bytes(server_pub_key); // Get the length of the public key
+    unsigned char *server_pub_key_bytes = new unsigned char[pub_key_len]; // Create a new byte array to store the public key
+    BN_bn2bin(server_pub_key, server_pub_key_bytes); // Convert the public key to bytes
 
     // Send server's public key to client
-    int pub_key_len = BN_num_bytes(pub_key);
-    unsigned char *pub_key_bytes = new unsigned char[pub_key_len];
-    BN_bn2bin(pub_key, pub_key_bytes);
+    send(clientSocket, server_pub_key_bytes, pub_key_len, 0); // Send the public key to the client
 
-    ssize_t sentBytes = send(clientSocket, pub_key_bytes, pub_key_len, 0);
-    if (sentBytes == -1) {
-        cerr << "Failed to send public key to client." << endl;
-        delete[] pub_key_bytes;
-        DH_free(dh);
-        close(clientSocket);
+    // Receive client's public key
+    unsigned char client_pub_key_bytes[2048]; // Create a new byte array to store the client's public key
+    ssize_t bytesReceived = recv(clientSocket, client_pub_key_bytes, sizeof(client_pub_key_bytes), 0); // Receive the client's public key
+    if (bytesReceived <= 0) { // Check if the public key was received successfully
+        cerr << "Failed to receive public key from client." << endl; // Print an error message
+        close(clientSocket); 
         close(serverSocket);
         return -1;
     }
-
-    cout << "Sent server's public key to client." << endl;
+    BIGNUM *client_pub_key = BN_bin2bn(client_pub_key_bytes, bytesReceived, nullptr); // Convert the client's public key to a BIGNUM
 
     // Compute shared secret
-    BIGNUM *client_pub_key = BN_bin2bn(buffer, bytesReceived, nullptr);
-    unsigned char shared_secret[EVP_MAX_KEY_LENGTH];
-
-    int secret_len = DH_compute_key(shared_secret, client_pub_key, dh);
-    if (secret_len <= 0) {
-        cerr << "Failed to compute shared secret." << endl;
-        delete[] pub_key_bytes;
-        DH_free(dh);
+    unsigned char shared_secret[EVP_MAX_KEY_LENGTH]; // Create a new byte array to store the shared secret
+    int secret_len = DH_compute_key(shared_secret, client_pub_key, dh); // Compute the shared secret using the client's public key and the DH object 
+    if (secret_len <= 0) { // Check if the shared secret was computed successfully
+        cerr << "Failed to compute shared secret." << endl; // Print an error message
         close(clientSocket);
         close(serverSocket);
         return -1;
     }
 
-    // Print shared secret for debugging
-    cout << "Client's shared secret: ";
-    for (int i = 0; i < secret_len; i++) {
-        printf("%02x", shared_secret[i]);
-    }
-    cout << endl;
+    cout << "Shared secret computed." << endl; // Print a message to the console 
+    delete[] server_pub_key_bytes; // Delete the server's public key bytes 
 
-    cout << "Shared secret computed." << endl;
+    // Main chat loop
+    while (true) {
+        // Receive IV and encrypted message from client
+        unsigned char iv[EVP_MAX_IV_LENGTH]; // Create a new byte array to store the  Initialization Vector
+        ssize_t ivReceived = recv(clientSocket, iv, sizeof(iv), 0); // Receive the Initialization Vector from the client
+        if (ivReceived <= 0) break; // Check if the IV was received successfully
 
-    // Receive IV and encrypted message
-    unsigned char iv[EVP_MAX_IV_LENGTH];
-    unsigned char encrypted_message[128];
+        unsigned char encrypted_message[128]; // Create a new byte array to store the encrypted message
+        ssize_t encrypted_len = recv(clientSocket, encrypted_message, sizeof(encrypted_message), 0); // Receive the encrypted message from the client
+        if (encrypted_len <= 0) break; // Check if the encrypted message was received successfully
 
-    ssize_t ivReceived = recv(clientSocket, iv, sizeof(iv), 0);
-    if (ivReceived <= 0) {
-        cerr << "Failed to receive IV from client." << endl;
-        delete[] pub_key_bytes;
-        DH_free(dh);
-        close(clientSocket);
-        close(serverSocket);
-        return -1;
-    }
+        // Decrypt the message
+        unsigned char decrypted_message[128]; // Create a new byte array to store the decrypted message
+        EVP_CIPHER_CTX *decrypt_ctx = EVP_CIPHER_CTX_new(); // Create a new EVP cipher context 
+        EVP_DecryptInit_ex(decrypt_ctx, EVP_aes_256_cbc(), nullptr, shared_secret, iv); // Initialize the decryption context 
+        int len, decrypted_len; // Create variables to store the length of the decrypted message
 
-    ssize_t encrypted_len = recv(clientSocket, encrypted_message, sizeof(encrypted_message), 0);
-    if (encrypted_len <= 0) {
-        cerr << "Failed to receive encrypted message from client." << endl;
-        delete[] pub_key_bytes;
-        DH_free(dh);
-        close(clientSocket);
-        close(serverSocket);
-        return -1;
-    }
+        if (EVP_DecryptUpdate(decrypt_ctx, decrypted_message, &len, encrypted_message, encrypted_len) == 1) { // Decrypt the message
+            decrypted_len = len; // Store the length of the decrypted message
+            if (EVP_DecryptFinal_ex(decrypt_ctx, decrypted_message + len, &len) == 1) { // Finalize the decryption process 
+                decrypted_len += len; // Update the length of the decrypted message
+                decrypted_message[decrypted_len] = '\0'; // Add a null terminator to the decrypted message
+                cout << "Client: " << decrypted_message << endl; // Print the decrypted message to the console
+            } 
+        } 
+        EVP_CIPHER_CTX_free(decrypt_ctx); // Free the decryption context
 
-    // Decrypt the message using the shared secret and received IV
-    unsigned char decrypted_message[128];
-    EVP_CIPHER_CTX *decrypt_ctx = EVP_CIPHER_CTX_new();
-    EVP_DecryptInit_ex(decrypt_ctx, EVP_aes_256_cbc(), nullptr, shared_secret, iv);
+        // Get server message and send to client
+        cout << "Server: "; // Print a message to the console 
+        string message; // Create a new string to store the message 
+        getline(cin, message); 
 
-    int len, decrypted_len;
-    if (EVP_DecryptUpdate(decrypt_ctx, decrypted_message, &len, encrypted_message, encrypted_len) != 1) {
-        cerr << "Decryption failed during update." << endl;
-        ERR_print_errors_fp(stderr);
-        EVP_CIPHER_CTX_free(decrypt_ctx);
-        delete[] pub_key_bytes;
-        DH_free(dh);
-        close(clientSocket);
-        close(serverSocket);
-        return -1;
-    }
-    decrypted_len = len;
+        if (message == "exit") break; // Check if the user wants to exit
 
-    if (EVP_DecryptFinal_ex(decrypt_ctx, decrypted_message + len, &len) != 1) {
-        cerr << "Decryption failed during finalization." << endl;
-        ERR_print_errors_fp(stderr);
-        EVP_CIPHER_CTX_free(decrypt_ctx);
-        delete[] pub_key_bytes;
-        DH_free(dh);
-        close(clientSocket);
-        close(serverSocket);
-        return -1;
-    }
+        // Encrypt and send the server message to the client
+        unsigned char iv_out[EVP_MAX_IV_LENGTH]; // Create a new byte array to store the Initialization Vector
+        RAND_bytes(iv_out, sizeof(iv_out)); // Generate a random IV 
+        EVP_CIPHER_CTX *encrypt_ctx = EVP_CIPHER_CTX_new(); // Create a new EVP cipher context 
+        EVP_EncryptInit_ex(encrypt_ctx, EVP_aes_256_cbc(), nullptr, shared_secret, iv_out); // Initialize the encryption context
 
-    decrypted_len += len;
-    decrypted_message[decrypted_len] = '\0'; // Null-terminate decrypted message
+        unsigned char encrypted_out[128]; // Create a new byte array to store the encrypted message
+        int encrypted_out_len; // Create a variable to store the length of the encrypted message
+        EVP_EncryptUpdate(encrypt_ctx, encrypted_out, &len, (unsigned char*)message.c_str(), message.size()); // Encrypt the message
+        encrypted_out_len = len; // Store the length of the encrypted message
 
-    cout << "Decrypted message: " << decrypted_message << endl;
+        EVP_EncryptFinal_ex(encrypt_ctx, encrypted_out + len, &len); // Finalize the encryption
+        encrypted_out_len += len; // Update the length of the encrypted message
+
+        send(clientSocket, iv_out, sizeof(iv_out), 0); // Send the IV to the client
+        send(clientSocket, encrypted_out, encrypted_out_len, 0); // Send the encrypted message to the client
+
+        EVP_CIPHER_CTX_free(encrypt_ctx); // Free the encryption context
+    } 
 
     // Cleanup
-    EVP_CIPHER_CTX_free(decrypt_ctx);
-    delete[] pub_key_bytes;
-    DH_free(dh);
-    close(clientSocket);
-    close(serverSocket);
+    DH_free(dh); // Free the DH object
+    close(clientSocket); // Close the client socket
+    close(serverSocket); // Close the server socket
 
     return 0;
 }
